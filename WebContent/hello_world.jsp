0e050fe38740f87c9efc83490820ebea97f95aa8
itacademy <itacademy@192.168.1.8>
2012-09-05 21:34:20 +0900

    MVCモデル

---
+++ WebContent/hello_world.jsp
@@ -0,0 +1,9 @@
+<%@page contentType="text/html; charset=utf-8" %>
+<%@page import="java.util.Date" %>
+
+<!DOCTYPE html>
+
+<meta charset="utf=8">
+<title>Hello world JSP</title>
+
+<p>Hello World at <%= new Date() %></p>
---
+++ src/jp/itacademy/samples/web/mvc/counter/CounterServlet.java
@@ -0,0 +1,30 @@
+package jp.itacademy.samples.web.mvc.counter;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/mvc/counter")
+public class CounterServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest req,
+			HttpServletResponse res) throws ServletException, IOException {
+		
+		HttpSession session = req.getSession();
+		Counter counter = (Counter) session.getAttribute("counter");
+		
+		if (counter == null) {
+			counter = new Counter();
+			session.setAttribute("counter", counter );
+		}
+		counter.inc();
+		
+		req.getRequestDispatcher("/WEB-INF/mvc/counter/result.jsp")
+		.forward(req, res);
+	}
+
+}
